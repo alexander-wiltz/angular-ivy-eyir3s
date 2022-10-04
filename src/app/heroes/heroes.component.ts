4c451e0f38b21f705497ab71d353371524042169
alexander-wiltz <64357568+alexander-wiltz@users.noreply.github.com>
2022-10-04 15:18:53 +0200

    5 files modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -22,6 +21,9 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    //this.heroes = this.heroService.getHeroes();
+    this.heroService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = this.heroes));
   }
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
+  { id: 12, name: 'Dr. Nice', age: 66 },
+  { id: 13, name: 'Bombasto', age: 42 },
+  { id: 14, name: 'Celeritas', age: 156 },
+  { id: 15, name: 'Magneta', age: 4 },
+  { id: 16, name: 'RubberMan', age: 88 },
+  { id: 17, name: 'Dynama', age: 1337 },
+  { id: 18, name: 'Dr. IQ', age: 56 },
+  { id: 19, name: 'Magma', age: 63 },
+  { id: 20, name: 'Tornado', age: 78 },
 ];
